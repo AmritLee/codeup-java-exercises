e06e85cf93f942ee78cc142ffae1434996f2b64a
Chan Lee <Chanaomrithlee@gmail.com>
2023-12-13 07:50:02 -0800

    completed exercise

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,50 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+
+        int myFavoriteNumber = 9;
+        System.out.println((myFavoriteNumber));
+
+        String myString = "Hello, this is a string.";
+//            myString = 'S';
+//            myString = 3.1459;
+            System.out.println(myString);
+
+            float myNumber;
+            myNumber = 3.14F;
+//            myNumber = 123L;
+//            myNumber= 123;
+            System.out.println(myNumber);
+
+//        int x = 5;
+//        System.out.println(x++);
+//        System.out.println(++x);
+//        System.out.println(x);
+
+        int classicNum = 9;
+
+//        String theNumberThree = "three";
+//        Object o = theNumberThree;
+//        int three = (int) o;
+
+//        int three = (int) "three";
+
+//        int x = 4;
+//        x += 5;
+
+//        int x = 3;
+//        int y = 4;
+//        y *= x;
+
+//        int x = 10;
+//        int y = 2;
+//        x /= y;
+//        y -= x;
+//        System.out.println(x);
+//        System.out.println(y);
+
+        int bigInt = Integer.MAX_VALUE + 1;
+        System.out.println(bigInt);
+
     }
 }
