90a0a1a14105170ef2b41549a6c97ae1dc956f10
Chan Lee <Chanaomrithlee@gmail.com>
2023-12-18 20:51:06 -0800

    StringExercise

--- src/StringExercise.java
+++ src/StringExercise.java
@@ -5,5 +5,11 @@ public class StringExercise {
 
         String message2 = "Check \"this\" out!,\"s inside of \"s!";
         System.out.println(message2);
+
+        String message3 = "In windows, the main drive is usually C:\\";
+        System.out.println(message3);
+
+        String message4 = "I can do backslashes \\, double backslashes \\\\, and the amazing triple backslashes \\\\\\";
+        System.out.println(message4);
     }
 }
